15cb778dbea312496160870ff44f28aa840114bb
Simonbackman <42805407+Simonbackman@users.noreply.github.com>
2019-03-27 12:37:18 +0100

    Gjort design på första sidan

---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+
+ <footer>
+    
+     <p> information gathered from <a href="https://www.marvel.com"/> Marvel's official site</a> </p>     
+     
+</footer>
---
+++ templates/nav.php
@@ -0,0 +1,6 @@
+<nav>
+    <a href="home.php"><div class="nav_item">Hem</div></a>
+    <a href="about.php"><div class="nav_item">About</div></a>
+    <a href="contactme.php"><div class="nav_item">Kontakta mig</div></a>
+    <a href="filmer.php"><div class="nav_item">Filmer</div></a>
+</nav>
