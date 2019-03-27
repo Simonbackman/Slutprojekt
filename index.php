7b38d01517e0a597696f95d9f666a614ab7f71c7
Simonbackman <42805407+Simonbackman@users.noreply.github.com>
2019-03-27 13:58:48 +0100

    ändrat bild/css

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<link rel="stylesheet" type="text/css" href="css.css"/>
     </head>
-    
     <body>
         <header class="header">
             <h1>Thai Tanic</h1>
@@ -17,27 +16,20 @@
             <a href="contact.php"><div class="nav_item">Kontakta oss</div></a>
         </nav>
         <Section class="section">
-
             <div class="tider">
                 <h2>Välkommen</h2>
                 <p class="opptider"><i>Öppetider:</p>
                 <p>Mån-Fre:12:00-21:00<br/>
                 Lör-Sön:12:00-19:30</i></p>
             </div>
-
-        
-            
-            <img class="bord" src="bilder/bord.jpg"/>
-            <img class="mat" src="bilder/mat.jpg"/>
+        <a href="bokning.php">
+            <img class="bord" src="bilder/bord.jpg" alt="bokning"/>
+        </a>
+        <a href="mat.php">
+            <img class="mat" src="bilder/mat.jpg" alt="rätter"/>
+        </a>
         </Section>
-
-
     </body>
-
-
-
-    
-
     <footer class="footer">
         <p>Copyright © 2019 Thai Tanic AB. Alla rättigheter reserverade.<br/> Org.nummer: 565432181-32159 </p> 
     </footer>
