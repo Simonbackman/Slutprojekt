46acc7dfb0c60b4b4339dc38be9e16449c938b78
Simonbackman <42805407+Simonbackman@users.noreply.github.com>
2019-03-28 10:45:46 +0100

    Gjort db rätter + bilder på sidan

--- databas/fooddb.sql
+++ databas/fooddb.sql
@@ -7,9 +7,9 @@ CREATE TABlE dishes (id int(8)PRIMARY KEY AUTO_INCREMENT, name varchar(32), pric
 
 
 INSERT INTO dishes (id, name, price, laktos, gluten, notter, spicy, img)
-Values  (1,'karaage',50,1,0,0,2,'karaage.jpg'),
-        (2,'noodle',45,0,1,0,1,'noodlejpg'),
-        (3,'pad_thai_cuisine',49,1,1,0,0,'pad_thai_cuisine.jpg'),
-        (4,'pad_thai_curry',49,1,1,1,2,'pad_thai_curry.jpg'),
-        (5,'niyom',59,0,0,1,3,'niyom.png'),
-        (6,'fried_rice',70,1,0,0,1,'fried_rice.png');
+Values  (1,'Karaage',50,1,0,0,2,'karaage.jpg'),
+        (2,'Noodles',45,0,1,0,1,'noodles.jpg'),
+        (3,'Pad thai cuisine',49,1,1,0,0,'pad_thai_cuisine.jpg'),
+        (4,'Pad thai curry',49,1,1,1,2,'pad_thai_curry.jpg'),
+        (5,'Niyom',59,0,0,1,3,'niyom.jpg'),
+        (6,'Fried ice',70,1,0,0,1,'fried_rice.jpg');
--- mat.php
+++ mat.php
@@ -15,7 +15,32 @@
             <a href="bokning.php"><div class="nav_item">Bokning</div></a>
             <a href="contact.php"><div class="nav_item">Kontakta oss</div></a>
         </nav>
-        <Section class="section">        
+        <Section class="section">
+            
+                <?php
+    
+                    $dbc = mysqli_connect("localhost","root","","food");
+
+                    $query = "SELECT * FROM dishes";
+
+                    mysqli_query($dbc,"SET NAMES utf8");
+
+                    $result = mysqli_query($dbc,$query);
+
+                    while($row = mysqli_fetch_array($result)){
+                    ?>
+                        <div class="food_container">
+                            <img class="food_img" src="bilder/<?php echo $row['img'];?>"/>
+                            <p class="food_name"><?php echo $row['name']; ?></p>
+                            <p class="food_price"><?php echo $row['price']; ?>kr</p>
+                            <p class="food_laktos"><?php echo $row['laktos']; ?></p>
+                            <p class="food_gluten"><?php echo $row['gluten']; ?></p>
+                            <p class="food_notter"><?php echo $row['notter']; ?></p>
+                            <p class="food_spicy"><?php echo $row['spicy']; ?></p>
+                        </div>
+                <?php
+                }
+            ?>
         </Section>
     </body>
     <footer class="footer">
