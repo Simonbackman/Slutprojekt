6b15c087e52a5af23193d658a499796acb48e536
Simonbackman <42805407+Simonbackman@users.noreply.github.com>
2019-03-28 08:47:18 +0100

    börjat på db

---
+++ databas/fooddb.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE food 
+
+use food 
+
+CREATE TABlE dishes (id int(8) PRIMARY KEY AUTO_INCREMENT, name varchar(32),price int(8), laktos int(8), gluten int(8), notter int(8),spicy int(8));
--- mat.php
+++ mat.php
@@ -15,19 +15,7 @@
             <a href="bokning.php"><div class="nav_item">Bokning</div></a>
             <a href="contact.php"><div class="nav_item">Kontakta oss</div></a>
         </nav>
-        <Section class="section">
-        Den ursprungliga Lorem Ipsum-texten, använd sedan 1500-talet
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-Sektion 1.10.32 av "de Finibus Bonorum et Malorum", författad av Cicero 45 år före år 0.
-"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-
-H. Rackham's översättning från 1914
-
-Sektion 1.10.33 av "de Finibus Bonorum et Malorum", författad av Cicero 45 år före år 0.
-
-H. Rackham's översättning från 1914
-        
+        <Section class="section">        
         </Section>
     </body>
     <footer class="footer">
