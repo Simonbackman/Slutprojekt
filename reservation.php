341df92c9564dba0dfcd0525a4d0544eb4d164ce
Simonbackman <42805407+Simonbackman@users.noreply.github.com>
2019-05-09 09:12:39 +0200

    -

--- reservation.php
+++ reservation.php
@@ -23,9 +23,10 @@ mysqli_query($dbc_reservation,$query);
 }
 else{
      $_SESSION['error_msg'] = "Felaktig information";
-    header("Location: bokning.php");
+      
+     header("Location: bokning.php");
     
 }
-header("location:bokning.php");
+ //header("location:bokning.php");
  echo mysqli_error($dbc_reservation);
 ?>
